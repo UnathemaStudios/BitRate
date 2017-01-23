0576e8814a162e7cb560e8fd447a496016d1039d
George <georgekosmidis1@yahoo.gr>
2017-01-23 15:15:20 +0200

    SubMenu colors + more human like

--- app/src/main/java/com/example/georg/radiostreameralt/RecordFragment.java
+++ app/src/main/java/com/example/georg/radiostreameralt/RecordFragment.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.vstechlab.easyfonts.EasyFonts;
 
 
 /**
@@ -16,6 +19,7 @@ import android.widget.Button;
  */
 public class RecordFragment extends Fragment {
 
+    RecordingNow recordingNow;
 
     public RecordFragment() {
         // Required empty public constructor
@@ -24,6 +28,8 @@ public class RecordFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        recordingNow = new RecordingNow();
     }
 
     @Override
@@ -32,18 +38,29 @@ public class RecordFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_record, container, false);
 
-        Button btRecordNow = (Button)view.findViewById(R.id.record_now_button);
-        Button btSchedule = (Button)view.findViewById(R.id.schedule_button);
-        Button btFolder = (Button)view.findViewById(R.id.folder_button);
+        final ImageButton btRecordNow = (ImageButton) view.findViewById(R.id.record_now_button);
+        final ImageButton btSchedule = (ImageButton)view.findViewById(R.id.schedule_button);
+        final ImageButton btFolder = (ImageButton)view.findViewById(R.id.folder_button);
+        //First Page Immidiate transaction without animation
+        FragmentManager manager = getFragmentManager();
+        manager.beginTransaction()
+                .replace(R.id.record_layout_for_fragments, recordingNow).commit();
+        btRecordNow.setSelected(true);
+        btRecordNow.setEnabled(false);
 
         btRecordNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RecordingNow recordingNow = new RecordingNow();
                 FragmentManager manager = getFragmentManager();
                 manager.beginTransaction()
                         .setCustomAnimations(R.anim.slide_in_from_left,R.anim.slide_out_from_left)
                         .replace(R.id.record_layout_for_fragments, recordingNow).commit();
+                btRecordNow.setSelected(true);
+                btRecordNow.setEnabled(false);
+                btFolder.setSelected(false);
+                btFolder.setEnabled(true);
+                btSchedule.setSelected(false);
+                btSchedule.setEnabled(true);
             }
         });
 
@@ -55,6 +72,12 @@ public class RecordFragment extends Fragment {
                 manager.beginTransaction()
                         .setCustomAnimations(R.anim.slide_in_from_left,R.anim.slide_out_from_left)
                         .replace(R.id.record_layout_for_fragments, schRecord).commit();
+                btRecordNow.setSelected(false);
+                btRecordNow.setEnabled(true);
+                btFolder.setSelected(false);
+                btFolder.setEnabled(true);
+                btSchedule.setSelected(true);
+                btSchedule.setEnabled(false);
             }
         });
 
@@ -66,6 +89,12 @@ public class RecordFragment extends Fragment {
                 manager.beginTransaction()
                         .setCustomAnimations(R.anim.slide_in_from_left,R.anim.slide_out_from_left)
                         .replace(R.id.record_layout_for_fragments, folderRecordings).commit();
+                btRecordNow.setSelected(false);
+                btRecordNow.setEnabled(true);
+                btFolder.setSelected(true);
+                btFolder.setEnabled(false);
+                btSchedule.setSelected(false);
+                btSchedule.setEnabled(true);
             }
         });
 
